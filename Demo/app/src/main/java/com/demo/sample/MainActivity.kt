8cb38a1d742a89127fe1f6e3587270dd9561e02d
devrath <devrath.ad@cybernetyx.com>
2020-05-30 15:08:58 +0530

    added main layout

--- Demo/app/src/main/java/com/demo/sample/MainActivity.kt
+++ Demo/app/src/main/java/com/demo/sample/MainActivity.kt
@@ -2,11 +2,21 @@ package com.demo.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.demo.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    //Binding reference
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textId.setOnClickListener {
+            Toast.makeText(this,"Hello",Toast.LENGTH_LONG).show()
+        }
     }
 }
