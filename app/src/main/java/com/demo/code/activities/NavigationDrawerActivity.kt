0642532b5f45ff0aadf26c8f3a8c6778d076b7fd
Devrath AD <devrathad@mplgaming.com>
2021-03-12 21:37:21 +0530

    d

---
+++ app/src/main/java/com/demo/code/activities/NavigationDrawerActivity.kt
@@ -0,0 +1,12 @@
+package com.demo.code.activities
+
+import android.os.Bundle
+import com.demo.code.R
+import com.demo.code.base.BaseActivity
+
+class NavigationDrawerActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
