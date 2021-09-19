5303097ce2612f643306104782ee1f2b245ddc59
Devrath <devrath.dev595@gmail.com>
2021-09-19 13:37:24 +0530

    m

--- app/src/main/java/com/demo/code/dataStore/util/DataManager.kt
+++ app/src/main/java/com/demo/code/dataStore/util/DataManager.kt
@@ -1,9 +1,11 @@
 package com.demo.code.dataStore.util
 
 import android.content.Context
-import androidx.datastore.preferences.createDataStore
-import androidx.datastore.preferences.edit
-import androidx.datastore.preferences.preferencesKey
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -11,27 +13,23 @@ import kotlinx.coroutines.flow.map
 const val DATA_STORE_NAME = "user_prefs"
 const val DATA_KEY = "data_key"
 
-class DataManager(context: Context) {
+class DataManager(val context: Context) {
 
     // Create the dataStore and give it a name same as shared preferences
-    private val dataStore = context.createDataStore(name = DATA_STORE_NAME)
-
+    val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = DATA_STORE_NAME)
+    
     // Create some keys we will use them to store and retrieve the data
     companion object {
-        val DATA_KEY_KEY = preferencesKey<String>(DATA_KEY)
+        val DATA_KEY_KEY = stringPreferencesKey(DATA_KEY)
     }
 
     // Store user data
     suspend fun storeData(data: String) {
-        dataStore.edit {
-            it[DATA_KEY_KEY] = data
-            // here it refers to the preferences we are editing
-        }
+        context.dataStore.edit { it[DATA_KEY_KEY] = data }
     }
 
     // Retrieve the data
-    val dataAsFlow: Flow<String> = dataStore.data.map {
-        it[DATA_KEY_KEY] ?: ""
-    }
+    val dataAsFlow: Flow<String> = context.dataStore.data
+        .map { preferences -> preferences[DATA_KEY_KEY] ?: "" }
 
 }
